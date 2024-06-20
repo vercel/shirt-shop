48aa5080849f1edf7acbeb6b104275ad0a3912db
Dominik Ferber <dominik.ferber@gmail.com>
2024-06-20 13:18:53 +0300

    remove FlagsReporter

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import { Analytics } from "@vercel/analytics/next";
-import { FlagsReporter } from "@vercel/flags/react";
 
 export function VercelAnalytics() {
   return (
     <>
       <Analytics />
-      <FlagsReporter
-        keys={["summer-sale", "free-delivery", "product-detail-reviews"]}
-      />
     </>
   );
 }
