5d9e66a421b27f06065fe4a1febf160fb8dda4ed
Dominik Ferber <dominik.ferber@gmail.com>
2024-06-20 13:58:04 +0300

    try hardcoding script src

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -5,7 +5,7 @@ import { Analytics } from "@vercel/analytics/next";
 export function VercelAnalytics() {
   return (
     <>
-      <Analytics />
+      <Analytics scriptSrc="https://analytics-script-git-dferber-fla-1138-get-rid-of-flagsre-f27b49.vercel.sh/v1/script.js" />
     </>
   );
 }
