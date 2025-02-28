e49162a6d4b7882a58bab7ad4451c44d6717c1a5
Dominik Ferber <dominik.ferber@gmail.com>
2025-02-28 11:12:15 +0200

    move

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/no-unresolved -- unsure
-import withVercelToolbar from '@vercel/toolbar/plugins/next';
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default withVercelToolbar()(nextConfig);
